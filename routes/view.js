7e5e6efe0452203de06b82f2c8ac9656706d639a
Tablizo <t.tablizo@g07.fujitsu.local>
2016-07-22 07:00:13 +0800

    commit changes

--- routes/view.js
+++ routes/view.js
@@ -4,8 +4,16 @@ var mysql = require('mysql');
 var blobStream = require('blob-stream');
 var fs = require('fs');
 var PDFDocument = require('pdfkit');
+var doc = new PDFDocument();
 
 var id = "153629";
+
+eName = 'Employee Name'
+headerQuali = 'Summary of Qualifications'
+headerSpec = 'Areas of Specialization'
+headertrain = 'Education and Training'
+headerCareer = 'Career Sypnosis'
+headerExp = 'Project Experience'
 
 // var mysql = require('mysql');
 /*
@@ -33,31 +41,46 @@ function Project(id, name) {
 */
 
 router.get('/', function(req, res, next) {
-	console.log('yyyyyyyyy init');
+	// console.log('yyyyyyyyy init');
 	
-	var objConn = createConn();
-	objConn.query('SELECT * FROM basic_information where empid = ? AND active_flag = ?', ['120698', 'Y'], function(error, rows) {
-		console.log('Basic Information Data');
-		console.log(rows.length);
+	// var objConn = createConn();
+	// objConn.query('SELECT * FROM basic_information where empid = ? AND active_flag = ?', ['120698', 'Y'], function(error, rows) {
+	// 	console.log('Basic Information Data');
+	// 	console.log(rows.length);
 		
-		var doc = new PDFDocument();
-		//doc.pipe(fs.createWriteStream('file.pdf'));
-		doc.text(rows[0].FIRSTNAME + ' ' + rows[0].MIDDLENAME + ' ' + rows[0].LASTNAME, 100,100);
+	// 	var doc = new PDFDocument();
+	// 	//doc.pipe(fs.createWriteStream('file.pdf'));
+	// 	doc.text(rows[0].FIRSTNAME + ' ' + rows[0].MIDDLENAME + ' ' + rows[0].LASTNAME, 100,100);
 
-		doc.text('WeServ Systems International, Inc.',100,110);
+	// 	doc.text('WeServ Systems International, Inc.',100,110);
 
 
-		doc.pipe(res);
-		doc.end();
-  });
-});
+	// 	doc.pipe(res);
+	// 	doc.end();
 
-router.get('/:id', function(req, res, next) {
-	var doc = new PDFDocument();
-	//doc.pipe(fs.createWriteStream('file.pdf'));
-	doc.text('Hello World and World again!!',100,100);
-	doc.pipe(res);
-	doc.end();
+    //doc.pipe(fs.createWriteStream('file.pdf'));
+    doc.text('Hello World and World again!!',100,100);
+    doc.pipe(res);
+    doc.end();
+    });
+
+    router.get('/:id', function(req, res, next) {
+    doc.pipe(fs.createWriteStream('file.pdf'));
+    doc.fontSize(20);
+    doc.text(eName, {
+      width:410,
+      align:'center'
+     });
+
+    doc.fontSize(12);
+    doc.text(headerQuali,100,90);
+    doc.text(headerSpec,100,120);
+    doc.text(headertrain,100,390);
+    doc.text(headerCareer,100,490);
+    doc.text(headerExp,100,590);
+    doc.pipe(res);
+    doc.end();
+
 });
 
 router.post('/:id', function(req, res) {
